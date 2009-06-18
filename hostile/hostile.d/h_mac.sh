bf6de8c11aacd7d9d92a6973e71af1a33d9662f0
Mr Parkinson <mparkinson@chat.ru>
2009-06-18 14:06:53 +0000

    add current development version

---
+++ hostile/hostile.d/h_mac.sh
@@ -0,0 +1,23 @@
+# h_mac
+
+h_mac_get() {
+	local IF=$1
+	macchanger --show $IF | sed -e 's,\(.*\) \([0-9a-f\]\+:[0-9a-f]\+:[0-9a-f]\+:[0-9a-f]\+:[0-9a-f]\+:[0-9a-f]\+\) \(.*\),\2,' 2>/dev/null \
+		|| h_error "can't get '$IF' mac address"
+}
+
+h_mac_set() {
+	local IF=$1
+	local MAC=$2
+	local opt
+	case $MAC in
+		auto)
+			opt="-a -e"
+			;;
+		*)
+			opt="-m $MAC"
+	esac
+	ifconfig $IF down
+	macchanger $opt $IF >/dev/null 2>&1 \
+		|| h_error "can't set '$IF' mac address to '$MAC'"
+}
