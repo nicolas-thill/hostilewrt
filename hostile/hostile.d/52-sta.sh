fe62075a54ae642cc2e4cb6c45eef07c779435ab
Mr Parkinson <mparkinson@chat.ru>
2009-06-24 17:15:20 +0000

    misc STA fixes

--- hostile/hostile.d/52-sta.sh
+++ hostile/hostile.d/52-sta.sh
@@ -1,16 +1,18 @@
 # STA
 
+H_STA_UDHCPC_PID_F=$H_RUN_D/hostile-udhcpc.pid
+H_STA_UDHCPC_SCRIPT_F=/dev/null
+
 h_sta_start() {
 	local bssid=$1
 	local channel=$2
 	local essid=$3
-	local end=$4
+	local enc=$4
 	local key=$5
 	ifconfig $H_STA_IF down
 	iwconfig $H_STA_IF ap "$bssid"
 	iwconfig $H_STA_IF essid "$essid"
 	iwconfig $H_STA_IF enc "$enc"
 	iwconfig $H_STA_IF key "$key"
-	udhcpc -i $H_STA_IF -p $H_STA_PID_F -f >/dev/null 2>&1 &
-	H_STA_DHCPC=$!
+	udhcpc -f -i $H_STA_IF -p $H_STA_UDHCPC_PID_F -s $H_STA_UDHCPC_SCRIPT_F >/dev/null 2>&1 &
 }
