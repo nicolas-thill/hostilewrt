d950e63f0a4bd697cabe40f94aa50e40f0e0927e
Mr Parkinson <mparkinson@chat.ru>
2009-06-24 17:08:33 +0000

    move STA functions & variables to their own module

--- hostile.sh
+++ hostile.sh
@@ -271,21 +271,6 @@ h_hw_fini() {
 	}
 }
 
-h_sta_start() {
-	local bssid=$1
-	local channel=$2
-	local essid=$3
-	local end=$4
-	local key=$5
-	ifconfig $H_STA_IF down
-	iwconfig $H_STA_IF ap "$bssid"
-	iwconfig $H_STA_IF essid "$essid"
-	iwconfig $H_STA_IF enc "$enc"
-	iwconfig $H_STA_IF key "$key"
-	udhcpc -i $H_STA_IF -p $H_STA_PID_F -f >/dev/null 2>&1 &
-	H_STA_DHCPC=$!
-}
-
 h_hw_prepare() {
 	local plop
 	local rate
