275fafb812ba56ed7c09639df058537df789f113
Mr Parkinson <mparkinson@chat.ru>
2009-06-24 16:27:03 +0000

    use hooks for leds handling

--- hostile/hostile.d/11-leds.sh
+++ hostile/hostile.d/11-leds.sh
@@ -22,3 +22,31 @@ h_led_off() {
 	echo "none" > /sys/class/leds/$led/trigger
 	echo 0 > /sys/class/leds/$led/brightness
 }
+
+h_leds_on_app_started() {
+	h_led_off $H_LED_SUCCESS
+	h_led_blink $H_LED_WIP 500
+}
+
+h_leds_on_app_ended() {
+	h_led_off $H_LED_SUCCESS
+	h_led_off $H_LED_WIP
+}
+
+h_leds_on_wep_attack_started() {
+	h_led_blink $H_LED_WIP 500
+}
+
+h_leds_on_wep_attack_working() {
+	h_led_blink $H_LED_WIP 100
+}
+
+h_leds_on_wep_key_found() {
+	h_led_on $H_LED_SUCCESS
+}
+
+h_hook_register_handlers on_app_started h_leds_on_app_started
+h_hook_register_handlers on_app_ended h_leds_on_app_ended
+h_hook_register_handlers on_wep_attack_started h_leds_on_wep_attack_started
+h_hook_register_handlers on_wep_attack_working h_leds_on_wep_attack_working
+h_hook_register_handlers on_wep_key_found h_leds_on_wep_key_found
