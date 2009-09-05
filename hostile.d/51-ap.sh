2166acda5ae4fa24e615e1d6860226ec0b97477e
Mr Parkinson <mparkinson@chat.ru>
2009-09-05 14:39:45 +0000

    mode dict files to their own directory

--- hostile.d/51-ap.sh
+++ hostile.d/51-ap.sh
@@ -5,6 +5,8 @@ H_AP_DNSMASQ_PID_F=$H_RUN_D/hostile-dnsmasq.pid
 
 h_ap_start() {
 	local cmd
+	[ -n "$H_OP_MODE_ap" ] \
+		|| return
 	h_log "starting: AP"
 	h_log "using: $H_AP_IF/$H_AP_IP/$H_AP_NETMASK for AP"
 	ifconfig $H_AP_IF down
@@ -13,15 +15,22 @@ h_ap_start() {
 	cmd="dnsmasq -i $H_AP_IF -F $H_AP_DHCP_MIN,$H_AP_DHCP_MAX,$H_AP_DHCP_LEASE_TIME -l $H_AP_DNSMASQ_LEASE_F -x $H_AP_DNSMASQ_PID_F"
 	h_log "running: $cmd"
 	$cmd
-	[ $? -eq 0 ] && {
-		return 0
-	}
-	return 1
+	h_hook_register_handler on_app_ending h_ap_stop
+	return 0
 }
 
 h_ap_stop() {
-	local pid=$(cat $H_AP_DNSMASQ_PID_F 2>&1)
+	local pid
 	h_log "stopping: AP"
-	[ -n "$pid" ] && kill -TERM $pid >/dev/null 2>&1
-	ifconfig $H_AP_IF down
+	pid=$(cat $H_AP_DNSMASQ_PID_F 2>&1)
+	if [ -n "$pid" ]; then
+		kill -TERM $pid >/dev/null 2>&1
+		sleep 1
+		echo "" > $H_AP_DNSMASQ_PID_F
+		ifconfig $H_AP_IF down
+	fi
+	h_hook_unregister_handler on_app_ending h_ap_stop
+	return 0
 }
+
+h_hook_register_handler on_app_started h_ap_start
--- hostile.d/52-sta.sh
+++ hostile.d/52-sta.sh
@@ -4,15 +4,54 @@ H_STA_UDHCPC_PID_F=$H_RUN_D/hostile-udhcpc.pid
 H_STA_UDHCPC_SCRIPT_F=/dev/null
 
 h_sta_start() {
-	local bssid=$1
-	local channel=$2
-	local essid=$3
-	local enc=$4
-	local key=$5
-	ifconfig $H_STA_IF down
-	iwconfig $H_STA_IF ap "$bssid"
-	iwconfig $H_STA_IF essid "$essid"
-	iwconfig $H_STA_IF enc "$enc"
-	iwconfig $H_STA_IF key "$key"
-	udhcpc -f -i $H_STA_IF -p $H_STA_UDHCPC_PID_F -s $H_STA_UDHCPC_SCRIPT_F >/dev/null 2>&1 &
+	local bssid
+	local channel
+	local essid
+	local enc
+	local key
+	local cmd
+	h_log "starting: Client"
+	bssid=$1
+	channel=$2
+	essid=$3
+	enc=$4
+	key=$5
+	if [ $end = "WEP" ]; then
+		h_log "using: bssid=$bssid, essid='$essid' & key='$key' for client mode"
+		ifconfig $H_STA_IF down
+		iwconfig $H_STA_IF ap "$bssid"
+		iwconfig $H_STA_IF essid "$essid"
+		iwconfig $H_STA_IF enc "$enc"
+		iwconfig $H_STA_IF key "$key"
+		cmd="udhcpc -f -R -i $H_STA_IF -p $H_STA_UDHCPC_PID_F -s $H_STA_UDHCPC_SCRIPT_F"
+		h_log "running: $cmd"
+		$cmd >/dev/null 2>&1 &
+		cmd="iptables -t nat -A POSTROUTING -o $H_STA_IF -j MASQUERADE"
+		h_log "running: $cmd"
+		$cmd >/dev/null 2>&1
+		h_hook_register_handler on_app_ending h_sta_stop
+		h_hook_register_handler on_channel_changing h_sta_stop
+	else
+		h_log "no support for encryption '$enc' (yet), sorry!"
+	fi
+	return 0
+}
+
+h_sta_stop() {
+	local pid
+	local cmd
+	h_log "stopping: Client"
+	cmd="iptables -t nat D POSTROUTING -o $H_STA_IF -j MASQUERADE"
+	h_log "running: $cmd"
+	$cmd >/dev/null 2>&1
+	pid=$(cat $H_STA_UDHCPC_PID_F 2>&1)
+	if [ -n "$pid" ]; then
+		kill -TERM $pid >/dev/null 2>&1
+		sleep 1
+		echo "" > $H_STA_UDHCPC_PID_F
+		ifconfig $H_STA_IF down
+	fi
+	h_hook_unregister_handler on_app_ending h_sta_stop
+	h_hook_unregister_handler on_channel_changing h_sta_stop
+	return 0
 }
