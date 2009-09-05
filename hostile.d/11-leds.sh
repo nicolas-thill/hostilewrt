82e0621c1b42894a190422052d4a32f003e5a28b
Mr Parkinson <mparkinson@chat.ru>
2009-09-05 17:53:51 +0000

    remove more bachisms (thanks to loloster)

--- hostile.d/11-leds.sh
+++ hostile.d/11-leds.sh
@@ -4,21 +4,25 @@ H_LED_SUCCESS=gpio4
 H_LED_WIP=gpio7
 
 h_led_on() {
-	local led=$1
+	local led
+	led=$1
 	echo "none" > /sys/class/leds/$led/trigger
 	echo 255 > /sys/class/leds/$led/brightness
 }
 
 h_led_blink() {
-	local led=$1
-	local delay=$2
+	local led
+	local delay
+	led=$1
+	delay=$2
 	echo "timer" > /sys/class/leds/$led/trigger
 	echo $delay > /sys/class/leds/$led/delay_on
 	echo $delay > /sys/class/leds/$led/delay_off
 }
 
 h_led_off() {
-	local led=$1
+	local led
+	led=$1
 	echo "none" > /sys/class/leds/$led/trigger
 	echo 0 > /sys/class/leds/$led/brightness
 }
