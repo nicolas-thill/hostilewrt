e6185b78054b9b693eed2501ab48b3140ed501ad
Mr Parkinson <mparkinson@chat.ru>
2009-10-22 04:50:52 +0000

    hooks: fix handler removal when the name of the handler to be removed is also contained in other handler names

--- hostile/hostile.d/00-hooks.sh
+++ hostile/hostile.d/00-hooks.sh
@@ -13,7 +13,7 @@ h_hook_register_handler() {
 	handler=$2
 	hook_var="H_HOOK_$hook"
 	eval "handlers=\"\${$hook_var}\""
-	eval "$hook_var=\"$handlers $handler\""
+	eval "$hook_var=\"$handlers $handler \""
 }
 
 h_hook_unregister_handler() {
@@ -25,7 +25,7 @@ h_hook_unregister_handler() {
 	handler=$2
 	hook_var="H_HOOK_$hook"
 	eval "handlers=\"\${$hook_var}\""
-	eval "$hook_var=\"\$(echo $handlers | sed -e 's/$handler//')\""
+	eval "$hook_var=\"\$(echo $handlers | sed -e 's/ $handler / /')\""
 }
 
 h_hook_call_handlers() {
