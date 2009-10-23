cf26d60944c13ff56fec9bae942aaaa925ac9f97
Mr Parkinson <mparkinson@chat.ru>
2009-10-23 03:47:10 +0000

    add a verbosity level arg to the ''h_log'' function

--- hostile.d/51-ap.sh
+++ hostile.d/51-ap.sh
@@ -7,13 +7,13 @@ h_ap_start() {
 	local cmd
 	[ -n "$H_OP_MODE_ap" ] \
 		|| return
-	h_log "starting: AP"
-	h_log "using: $H_AP_IF/$H_AP_IP/$H_AP_NETMASK for AP"
+	h_log 1 "starting: AP"
+	h_log 1 "using: $H_AP_IF/$H_AP_IP/$H_AP_NETMASK for AP"
 	ifconfig $H_AP_IF down
 	iwconfig $H_AP_IF essid "$H_AP_ESSID"
 	ifconfig $H_AP_IF $H_AP_IP netmask $H_AP_NETMASK up
 	cmd="dnsmasq -i $H_AP_IF -F $H_AP_DHCP_MIN,$H_AP_DHCP_MAX,$H_AP_DHCP_LEASE_TIME -l $H_AP_DNSMASQ_LEASE_F -x $H_AP_DNSMASQ_PID_F"
-	h_log "running: $cmd"
+	h_log 2 "running: $cmd"
 	$cmd
 	h_hook_register_handler on_app_ending h_ap_stop
 	return 0
@@ -21,7 +21,7 @@ h_ap_start() {
 
 h_ap_stop() {
 	local pid
-	h_log "stopping: AP"
+	h_log 1 "stopping: AP"
 	pid=$(cat $H_AP_DNSMASQ_PID_F 2>&1)
 	if [ -n "$pid" ]; then
 		kill -TERM $pid >/dev/null 2>&1
