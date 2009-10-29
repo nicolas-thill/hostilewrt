3aaf3d04c2d599f800d1a2844c5acc3365940bd8
Mr Parkinson <mparkinson@chat.ru>
2009-10-29 17:01:59 +0000

    add a ''net_open'' module and move open network helper functions to it

--- hostile.sh
+++ hostile.sh
@@ -418,25 +418,6 @@ h_clean_run_d() {
 
 
 #
-# handle open networks
-#
-
-h_open_try_one_network() {
-	h_net_switch $1 || return 1
-	h_net_allowed || return 1
-	h_log 1 "found open network (bssid='$H_CUR_BSSID', channel=$H_CUR_CHANNEL, essid='$H_CUR_ESSID')"
-
-	[ "$H_OP_MODE_sta" = "1" ] && h_sta_try "OPEN" "off"
-}
-
-h_open_try_all_networks() {
-	for N in $(cat $H_NET_OPEN_F); do
-		h_open_try_one_network $N
-	done
-}
-
-
-#
 # handle WEP networks
 #
 
